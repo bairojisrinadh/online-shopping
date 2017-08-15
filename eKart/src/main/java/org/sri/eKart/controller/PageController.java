f2bc4e4f74db01f36003e7d9fc006b18c99308fe
bairojisrinadh <bairojisrinadh@gmail.com>
2017-08-15 17:39:47 +0530

    Successfully created Home Page for eKart

---
+++ eKart/src/main/java/org/sri/eKart/controller/PageController.java
@@ -0,0 +1,16 @@
+package org.sri.eKart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/","/home","/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greetings", "Welcome to Spring");
+		return mv;
+	}
+}
